30dcfe77ef8c05a4c50a170f85ab22c6d82555e0
Charbrown08 <carlos.moreno.jimenez@konecta-group.com>
2024-11-10 12:03:26 -0500

    [ADD]  interactive buttons and  location by the startup

--- src/services/messageHandler.js
+++ src/services/messageHandler.js
@@ -13,6 +13,17 @@ class MessageHandler {
         await whatsappService.sendMessage(message.from, response, message.id)
       }
       await whatsappService.markAsRead(message.id)
+    } else if (message?.type === 'interactive') {
+      const option = message?.interactive?.button_reply?.title
+        .toLowerCase()
+        .trim()
+      console.log(
+        '🚀 ~ MessageHandler ~ handleIncomingMessage ~ option:',
+        option,
+      )
+
+      await this.handleMenuOption(message.from, option)
+      await whatsappService.markAsRead(message.id)
     }
   }
 
@@ -37,19 +48,47 @@ class MessageHandler {
     const buttons = [
       {
         type: 'reply',
-        reply: { id: 'option_1', title: 'Agendar sweets' },
+        reply: { id: 'option_1', title: 'Agendar' },
       },
       {
         type: 'reply',
-        reply: { id: 'option_2', title: 'Ver sweets' },
+        reply: { id: 'option_2', title: 'Ver' },
       },
       {
         type: 'reply',
-        reply: { id: 'option_3', title: 'Consultar sweets' },
+        reply: { id: 'option_3', title: 'Consultar' },
+      },
+      {
+        type: 'reply',
+        reply: { id: 'option_4', title: 'Ubicar' },
       },
     ]
 
     await whatsappService.sendInteractiveButtons(to, menuMessage, buttons)
+  }
+
+  async handleMenuOption(to, option) {
+    let response
+
+    switch (option) {
+      case 'agendar':
+        response = 'Agendar sweets'
+        break
+      case 'ver':
+        response = 'Ver sweets'
+        break
+      case 'consultar':
+        response = 'Consultar sweets'
+        break
+      case 'ubicar':
+        response = 'Ubicar sweets'
+        break
+      default:
+        response =
+          'Disculpanos, No entendimos tu eleccion, por favor elige una de las opciones del menu'
+    }
+
+    await whatsappService.sendMessage(to, response)
   }
 }
 
