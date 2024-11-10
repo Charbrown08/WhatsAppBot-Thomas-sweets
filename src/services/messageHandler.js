46a693924fc17a4b0842bc7d6c080c9c374885ae
Charbrown08 <carlos.moreno.jimenez@konecta-group.com>
2024-11-10 02:18:43 -0500

    [ADD] config send message

--- src/services/messageHandler.js
+++ src/services/messageHandler.js
@@ -1,12 +1,47 @@
+// import whatsappService from './whatsappService.js'
+
+// class MessageHandler {
+//   async handleIncomingMessage(message) {
+//     if (message?.type === 'text') {
+//       const response = `Echo: ${message.text.body}`
+//       await whatsappService.sendMessage(message.from, response, message.id)
+//       await whatsappService.markAsRead(message.id)
+//     }
+//   }
+// }
+
+// export default new MessageHandler()
+
 import whatsappService from './whatsappService.js'
 
 class MessageHandler {
-  async handleIncomingMessage(message) {
+  async handleIncomingMessage(message, senderInfo) {
     if (message?.type === 'text') {
-      const response = `Echo: ${message.text.body}`
-      await whatsappService.sendMessage(message.from, response, message.id)
+      const incomingMessage = message.text.body.toLowerCase().trim()
+
+      if (this.isGreeting(incomingMessage)) {
+        await this.sendWelcomeMessage(message.from, message.id, senderInfo)
+      } else {
+        const response = `Echo: ${message.text.body}`
+        await whatsappService.sendMessage(message.from, response, message.id)
+      }
       await whatsappService.markAsRead(message.id)
     }
+  }
+
+  isGreeting(message) {
+    const greetings = ['hola', 'hello', 'hi', 'buenas tardes']
+    return greetings.includes(message)
+  }
+
+  getSenderName(senderInfo) {
+    return senderInfo.profile?.name || senderInfo.wa_id
+  }
+
+  async sendWelcomeMessage(to, messageId, senderInfo) {
+    const name = this.getSenderName(senderInfo)
+    const welcomeMessage = `Hola ${name}, Bienvenido a Thomas&Sweets, Tu tienda de dulces sensaciones en línea. ¿Que quieres probar hoy?`
+    await whatsappService.sendMessage(to, welcomeMessage, messageId)
   }
 }
 
